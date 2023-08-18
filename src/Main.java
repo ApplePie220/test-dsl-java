51479857b8292569ea4aca6a33760a96b243532d
crowlyy <crowly97@gmail.com>
2023-08-18 13:32:54 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        String planetScalePassword_api_token;
+        String password = "123456543";
+        String andpassword123 = "23456";
+        String snfPassword = "23456";
+        System.out.printf("Hello and welcome!");
+        planetScalePassword_api_token = "pscale_pw_d6kvuewnpziee30nfl3d8ldxdi8=4d21";
+        planetScalePassword_api_token = "pscale_tkn_ci7yg072sgqyxr=l30e_y2r1=11uic5m";
+        planetScalePassword_api_token = "pscale_oauth_132tp-ag=4set2_7yhibbj8m1-xcsk3y";
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
